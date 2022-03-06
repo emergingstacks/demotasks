7b83d4ce182aea01ffbffadab05926e5f67edb1b
Harshal Kulkarni <harshalk.91@gmail.com>
2022-03-06 09:20:23 +0530

    destroy created resources

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,27 +1,27 @@
-resource "aws_iam_role" "demo_api_task_execution_role" {
-  name               = "demo-api-task-execution-role"
-  assume_role_policy = data.aws_iam_policy_document.ecs_task_assume_role.json
-}
-
-data "aws_iam_policy_document" "ecs_task_assume_role" {
-  statement {
-    actions = ["sts:AssumeRole"]
-
-    principals {
-      type = "Service"
-      identifiers = ["ecs-tasks.amazonaws.com"]
-    }
-  }
-}
-
-# Normally we'd prefer not to hardcode an ARN in our Terraform, but since this is
-# an AWS-managed policy, it's okay.
-data "aws_iam_policy" "ecs_task_execution_role" {
-  arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
-}
-
-# Attach the above policy to the execution role.
-resource "aws_iam_role_policy_attachment" "ecs_task_execution_role" {
-  role       = aws_iam_role.demo_api_task_execution_role.name
-  policy_arn = data.aws_iam_policy.ecs_task_execution_role.arn
-}
+#resource "aws_iam_role" "demo_api_task_execution_role" {
+#  name               = "demo-api-task-execution-role"
+#  assume_role_policy = data.aws_iam_policy_document.ecs_task_assume_role.json
+#}
+#
+#data "aws_iam_policy_document" "ecs_task_assume_role" {
+#  statement {
+#    actions = ["sts:AssumeRole"]
+#
+#    principals {
+#      type = "Service"
+#      identifiers = ["ecs-tasks.amazonaws.com"]
+#    }
+#  }
+#}
+#
+## Normally we'd prefer not to hardcode an ARN in our Terraform, but since this is
+## an AWS-managed policy, it's okay.
+#data "aws_iam_policy" "ecs_task_execution_role" {
+#  arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+#}
+#
+## Attach the above policy to the execution role.
+#resource "aws_iam_role_policy_attachment" "ecs_task_execution_role" {
+#  role       = aws_iam_role.demo_api_task_execution_role.name
+#  policy_arn = data.aws_iam_policy.ecs_task_execution_role.arn
+#}
--- terraform/main.tf
+++ terraform/main.tf
@@ -1,34 +1,34 @@
-resource "aws_ecr_repository" "democi" {
-  name                 = "democi"
-  image_tag_mutability = "MUTABLE"
-
-  image_scanning_configuration {
-    scan_on_push = true
-  }
-}
-
-resource "aws_ecr_repository_policy" "democi-repo-policy" {
-  repository = aws_ecr_repository.democi.name
-  policy     = <<EOF
-  {
-    "Version": "2008-10-17",
-    "Statement": [
-      {
-        "Sid": "adds full ecr access to the demo repository",
-        "Effect": "Allow",
-        "Principal": "*",
-        "Action": [
-          "ecr:BatchCheckLayerAvailability",
-          "ecr:BatchGetImage",
-          "ecr:CompleteLayerUpload",
-          "ecr:GetDownloadUrlForLayer",
-          "ecr:GetLifecyclePolicy",
-          "ecr:InitiateLayerUpload",
-          "ecr:PutImage",
-          "ecr:UploadLayerPart"
-        ]
-      }
-    ]
-  }
-  EOF
-}
+#resource "aws_ecr_repository" "democi" {
+#  name                 = "democi"
+#  image_tag_mutability = "MUTABLE"
+#
+#  image_scanning_configuration {
+#    scan_on_push = true
+#  }
+#}
+#
+#resource "aws_ecr_repository_policy" "democi-repo-policy" {
+#  repository = aws_ecr_repository.democi.name
+#  policy     = <<EOF
+#  {
+#    "Version": "2008-10-17",
+#    "Statement": [
+#      {
+#        "Sid": "adds full ecr access to the demo repository",
+#        "Effect": "Allow",
+#        "Principal": "*",
+#        "Action": [
+#          "ecr:BatchCheckLayerAvailability",
+#          "ecr:BatchGetImage",
+#          "ecr:CompleteLayerUpload",
+#          "ecr:GetDownloadUrlForLayer",
+#          "ecr:GetLifecyclePolicy",
+#          "ecr:InitiateLayerUpload",
+#          "ecr:PutImage",
+#          "ecr:UploadLayerPart"
+#        ]
+#      }
+#    ]
+#  }
+#  EOF
+#}
