1132ffdd8dcebe41ca43ba4c2f4df6a47b3aff1a
Harshal Kulkarni <harshalk.91@gmail.com>
2022-03-06 08:02:22 +0530

    Add terraform code for ecs and added policies

---
+++ terraform/fargate.tf
@@ -0,0 +1,31 @@
+resource "aws_ecs_cluster" "demo-ecs-cluster" {
+  name = "ecs-cluster-for-demo"
+}
+resource "aws_ecs_task_definition" "demoapi_task" {
+   family = aws_ecs_service.demoapi_service.name
+   cpu = "256"
+   memory = "512"
+   requires_compatibilities = ["FARGATE"]
+   network_mode = "awsvpc"
+   container_definitions = <<EOF
+[
+{
+      "name": "sun-api",
+      "image": "745946109524.dkr.ecr.us-east-1.amazonaws.com/democi:latest",
+      "portMappings": [
+        {
+          "containerPort": 8080
+        }
+      ]
+}
+]
+EOF
+
+}
+
+resource "aws_ecs_service" "demoapi_service" {
+  name            = "demoapi"
+  task_definition = aws_ecs_task_definition.demoapi_task.arn
+  launch_type = "FARGATE"
+  desired_count = 1
+}
