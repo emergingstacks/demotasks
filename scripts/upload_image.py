8264f1e0d805245fe1f075122dc2b466687d088b
Harshal kulkarni <harshalk.91@gmail.com>
2022-03-06 07:44:36 +0530

    optimisation

--- scripts/upload_image.py
+++ scripts/upload_image.py
@@ -24,20 +24,22 @@ def build_docker_image(dest_dirc, docker_image_url):
     print("Building docker image")
     hash = random.getrandbits(128)
     client.images.build(path="/tmp/democi", tag=docker_image_url + ":latest")
+    client.images.build(path="/tmp/democi", tag="{}:{}".format(docker_image_url, hash))
+    return [str(hash), "latest"]
 
 
-def upload_image_to_ecr(docker_image_url):
+def upload_image_to_ecr(docker_image_url, image_tags):
     token = ecr_client.get_authorization_token()
     username, password = base64.b64decode(token['authorizationData'][0]['authorizationToken']).decode().split(':')
     registry = token['authorizationData'][0]['proxyEndpoint']
     auth_config = {'username': username, 'password': password}
-    print(username, password)
     print("logging to ec2")
     client.login(username, password, registry=registry)
     print("pushing the image")
-    client.images.push(docker_image_url, auth_config=auth_config)
+    for tag in image_tags:
+        client.images.push(docker_image_url+":"+tag, auth_config=auth_config)
 
 
 clone_repo(repo_url, destination_dir)
-# build_docker_image(destination_dir, docker_repo_url)
-# upload_image_to_ecr(docker_repo_url)
+image_tags = build_docker_image(destination_dir, docker_repo_url)
+upload_image_to_ecr(docker_repo_url, image_tags)
